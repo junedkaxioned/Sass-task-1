df32a0c7395ca4b2f3f5497d2a65d19cbf66c150
JKAX2022 <junedk@axioned.com>
2022-11-27 18:42:33 +0530

    Mobile Navbar function completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
-/* Author: Zishan Ansari */
+var html = document.querySelector('html');
+var hamBurger = document.querySelector('.hamburger');
+var navbar = document.querySelector('nav');
+
+
+// Mobile Navbar Function Start 
+hamBurger.addEventListener('click', function () {
+  html.classList.toggle('stop-scroll')
+  hamBurger.classList.toggle('active')
+  navbar.classList.toggle('active')
+})
+// Mobile Navbar Function End 
